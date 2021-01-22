d81f326f56a79e234201549c33e87e8e002c44de
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-22 17:08:39 +0000

    add about route

--- hello_blog/main/routes.py
+++ hello_blog/main/routes.py
@@ -7,3 +7,9 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def home():
     return render_template("main/home.html")
+
+
+@main.route("/about")
+def about():
+    return render_template("main.about.html",
+                           title="About Page")
