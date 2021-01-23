4f0cea358e97e7ee55fe5177732464a1d6034810
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-23 15:30:23 +0000

    create users blueprint

--- hello_blog/__init__.py
+++ hello_blog/__init__.py
@@ -5,7 +5,9 @@ def create_app():
     app = Flask(__name__)
 
     from hello_blog.main.routes import main
+    from hello_blog.users.routes import users
 
     app.register_blueprint(main)
+    app.register_blueprint(users)
 
     return app
