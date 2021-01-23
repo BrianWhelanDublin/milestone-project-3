c0d1578ef79ea2fbc11ca633f6f0fecf9136c72d
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-23 15:49:41 +0000

    create SignupForm Class

---
+++ hello_blog/users/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class SignupForm(FlaskForm):
+    username = StringField("Username",
+                           validators=[DataRequired(),
+                                       Length(min=3, max=20)])
+    email = StringField("Email",
+                        validators=[DataRequired(), Email()])
+    password = PasswordField("Password",
+                             validators=[DataRequired()])
+    confirm_password = PasswordField("Password",
+                                     validators=[DataRequired(),
+                                                 EqualTo("password")])
+    submit = SubmitField("Sign Up")
