dbdd84e21028d220198664184b098feeace87705
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-23 15:54:40 +0000

    create signup route

--- hello_blog/users/routes.py
+++ hello_blog/users/routes.py
@@ -1,4 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from hello_blog.users.forms import SignupForm
 
 
 users = Blueprint("users", __name__)
+
+
+@users.route("/signup")
+def signup():
+    form = SignupForm()
+    return render_template("users/signup.html",
+                           title="Sign Up",
+                           form=form)
