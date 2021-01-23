53e54e15dfca82cba57725611520673a6eb0a952
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-23 16:18:35 +0000

    add the config class to the create app function

--- hello_blog/__init__.py
+++ hello_blog/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask
+from hello_blog.config import Config
 
 
-def create_app():
+# cretes the app using the details from the config class in the config.py file
+def create_app(config_class=Config):
     app = Flask(__name__)
+    app.config.from_object(config_class)
 
+#  imports the blueprints from each route file
     from hello_blog.main.routes import main
     from hello_blog.users.routes import users
 
+#  register each blueprint to connect it with the app
     app.register_blueprint(main)
     app.register_blueprint(users)
 
+# then return the app which will run when the create_app
+# function is called in run.py
     return app
