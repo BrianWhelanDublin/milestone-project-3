87b4e803a4183b34697ab66f1fa86e1a48baa69a
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-24 12:35:33 +0000

    add functionality to save user to database

--- hello_blog/users/routes.py
+++ hello_blog/users/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, url_for, flash, redirect
 from hello_blog.users.forms import SignupForm
+from hello_blog.models import User
 
 
 users = Blueprint("users", __name__)
@@ -12,8 +13,15 @@ def signup():
     form = SignupForm()
     # checks if the form is valid using wtf validators
     if form.validate_on_submit():
+        # creates an instance of User class from form data
+        # and then hashes thier password
+        # and saves this user to the database in mongodb
+        user = User(username=form.username.data,
+                    email=form.email.data)
+        user.hash_password(form.password.data)
+        user.save()
         flash("User registered", "success")
-        return redirect(url_for("users.signup"))
+        return redirect(url_for("main.home"))
     return render_template("users/signup.html",
                            title="Sign Up",
                            form=form)
