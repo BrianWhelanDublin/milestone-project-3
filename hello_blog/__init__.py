c82dfaeaa81dd830ead36d6d354e4f6e0add998f
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-24 14:10:56 +0000

    add flask_login LoginManager

--- hello_blog/__init__.py
+++ hello_blog/__init__.py
@@ -2,10 +2,16 @@ from flask import Flask
 from hello_blog.config import Config
 from flask_mongoengine import MongoEngine
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 # create instances of imported extensions
 db = MongoEngine()
 bcrypt = Bcrypt()
+login_manager = LoginManager()
+# user will be redirected here if the try to accsess a page
+# with login_required and they havent logged in
+login_manager.login_view = "users.login"
+login_manager.login_message_category = "info"
 
 
 # cretes the app using the details from the config class in the config.py file
@@ -16,6 +22,7 @@ def create_app(config_class=Config):
     # init the extention instances to bind them to the app
     db.init_app(app)
     bcrypt.init_app(app)
+    login_manager.init_app(app)
 
     #  imports the blueprints from each route file
     from hello_blog.main.routes import main
