f4d0ce56303b711e8727da1098bca8ed08617e82
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-24 14:20:51 +0000

    add flask_login functionality

--- hello_blog/models.py
+++ hello_blog/models.py
@@ -1,9 +1,11 @@
-from hello_blog import db, bcrypt
+from hello_blog import db, bcrypt, login_manager
 from flask_mongoengine import BaseQuerySet
+# Usermixin adds the basic functions needed to the userclass for flask login
+from flask_login import UserMixin
 
 
 # creates the User class
-class User(db.Document):
+class User(db.Document, UserMixin):
     username = db.StringField(max_length=20, unique=True, required=True)
     email = db.StringField(max_length=100, unique=True, required=True)
     password = db.StringField(max_length=100)
@@ -21,3 +23,9 @@ class User(db.Document):
     # - the users passwords for the database
     def hash_password(self, password):
         self.password = bcrypt.generate_password_hash(password).decode("utf-8")
+
+
+# create the login_manager decorated function needed for flask login
+@login_manager.user_loader
+def load_user(user_id):
+    return User.objects(pk=user_id).first()
