91d2670d86eabec11da12f1f4787416bcb68f24c
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-24 11:55:36 +0000

    connect to mongo db using Flask-Mongoengine

--- hello_blog/__init__.py
+++ hello_blog/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
 from hello_blog.config import Config
+from flask_mongoengine import MongoEngine
+
+
+# create instances of imported extensions
+db = MongoEngine()
 
 
 # cretes the app using the details from the config class in the config.py file
@@ -7,14 +12,17 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-#  imports the blueprints from each route file
+    # init the extention instances to bind them to the app
+    db.init_app(app)
+
+    #  imports the blueprints from each route file
     from hello_blog.main.routes import main
     from hello_blog.users.routes import users
 
-#  register each blueprint to connect it with the app
+    #  register each blueprint to connect it with the app
     app.register_blueprint(main)
     app.register_blueprint(users)
 
-# then return the app which will run when the create_app
-# function is called in run.py
+    # then return the app which will run when the create_app
+    # function is called in run.py
     return app
--- hello_blog/config.py
+++ hello_blog/config.py
@@ -5,3 +5,7 @@ if os.path.exists("env.py"):
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
+    MONGODB_SETTINGS = {
+        'db': os.environ.get("MONGO_DBNAME"),
+        'host': os.environ.get("MONGO_URI")
+        }
