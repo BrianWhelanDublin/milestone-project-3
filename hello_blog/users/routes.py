a192876f3e144399a3383407f6d8443f3159379c
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-24 11:35:41 +0000

    add flash messages

--- hello_blog/users/routes.py
+++ hello_blog/users/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, flash, redirect
 from hello_blog.users.forms import SignupForm
 
 
@@ -6,10 +6,14 @@ users = Blueprint("users", __name__)
 
 
 # create the users route where users can sign up
-@users.route("/signup")
+@users.route("/signup", methods=["GET", "POST"])
 def signup():
     # use form created in users.forms.py
     form = SignupForm()
+    # checks if the form is valid using wtf validators
+    if form.validate_on_submit():
+        flash("User registered", "success")
+        return redirect(url_for("users.signup"))
     return render_template("users/signup.html",
                            title="Sign Up",
                            form=form)
