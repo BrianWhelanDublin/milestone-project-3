38590ffed2ebfc398b14511aa461f8cf503e2ab9
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-24 12:27:42 +0000

    create hash_password function

--- hello_blog/models.py
+++ hello_blog/models.py
@@ -6,9 +6,14 @@ from flask_mongoengine import BaseQuerySet
 class User(db.Document):
     username = db.StringField(max_length=20, unique=True, required=True)
     email = db.StringField(max_length=100, unique=True, required=True)
-    password = db.StringField(max_length=50)
+    password = db.StringField(max_length=100)
 
     meta = {
         "collection": "users",
         "queryset_class": BaseQuerySet
     }
+
+    # creates a function that uses flask-bcrypt to hash
+    # the users passwords for the database
+    def hash_password(self, password):
+        self.password = bcrypt.generate_password_hash(password).decode("utf-8")
