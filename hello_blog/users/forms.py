b168e556cf6f7dce2dd72ae38d4ae64ffdb4f450
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-25 13:37:06 +0000

    create UpdateAccount form

--- hello_blog/users/forms.py
+++ hello_blog/users/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import (DataRequired, Length, Email,
                                 EqualTo, ValidationError)
 from hello_blog.models import User
+from flask_login import current_user
 
 
 # creates the form and validaters for the user to sign up.
@@ -39,3 +41,30 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember_user = BooleanField("Remember Me")
     submit = SubmitField("Login")
+
+
+# create form for updating account
+class UpdateAccount(FlaskForm):
+    username = StringField("Username",
+                           validators=[DataRequired(),
+                                       Length(min=3, max=20)])
+    email = StringField("Email",
+                        validators=[DataRequired(), Email()])
+    user_image = FileField("Update Profile Image",
+                           validators=[FileAllowed(["jpg", "png"])])
+
+    # use custom validators to insure the username and email is unique
+    # only run if the user has changed the data
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.objects(username=username.data).first()
+            if user is not None:
+                raise ValidationError("Username already in use.\
+                    Please try another")
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.objects(email=email.data).first()
+            if user is not None:
+                raise ValidationError("Email already in use.\
+                    Please try another")
