40047a6f0264f8c1171af826b1d977898e398920
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-01-26 13:43:43 +0000

    start all_posts route

--- hello_blog/posts/posts_views.py
+++ hello_blog/posts/posts_views.py
@@ -1,4 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask_login import login_required
 
 
 posts = Blueprint("posts", __name__)
+
+
+# create the route for the main post page to show
+#  all recent posts to the user
+@posts.route("/posts")
+@login_required
+def all_posts():
+    return render_template("posts/all_posts.html",
+                           title="Recent Posts")
