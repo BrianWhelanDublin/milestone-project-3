615e64aab6bede4bd50c344deff103660a1b0997
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-02-01 09:24:49 +0000

    create error blueprint and initialize

--- hello_blog/__init__.py
+++ hello_blog/__init__.py
@@ -31,11 +31,13 @@ def create_app(config_class=Config):
     from hello_blog.main.main_views import main
     from hello_blog.users.users_views import users
     from hello_blog.posts.posts_views import posts
+    from hello_blog.errors.handlers import errors
 
     #  register each blueprint to connect it with the app
     app.register_blueprint(main)
     app.register_blueprint(users)
     app.register_blueprint(posts)
+    app.register_blueprint(errors)
 
     # then return the app which will run when the create_app
     # function is called in run.py
---
+++ hello_blog/errors/handlers.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, render_template
+
+
+errors = Blueprint("errors", __name__)
+
+
