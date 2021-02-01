b4e52f812c0d9647be020dd5c95b82426469cbbe
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-02-01 12:13:48 +0000

    add function to save category to database

--- hello_blog/admin/admin_views.py
+++ hello_blog/admin/admin_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, redirect, url_for, flash
 from flask_login import login_required, current_user
 from hello_blog.models import User, Post, Categories
 from hello_blog.admin.admin_forms import AddCategoryForm
@@ -20,12 +20,19 @@ def dashboard():
                            categories=categories)
 
 
-@admin.route("/add/category")
+@admin.route("/add/category", methods=["GET", "POST"])
 @login_required
 def add_category():
     if current_user.username != "admin":
         abort(403)
     form = AddCategoryForm()
+    if form.validate_on_submit():
+        category = Categories(
+            category_name=form.category_name.data
+        )
+        category.save()
+        flash("Category saved", "success")
+        return redirect(url_for('admin.dashboard'))
     return render_template("admin/add_category.html",
                            title="Add Category",
                            form=form)
