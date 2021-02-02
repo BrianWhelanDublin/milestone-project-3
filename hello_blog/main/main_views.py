0014c6fd462d73a44700c58ab1b2b9a1dcee9f8c
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-02-02 16:23:26 +0000

    add posts to home page

--- hello_blog/main/main_views.py
+++ hello_blog/main/main_views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import current_user
+from hello_blog.models import Post
 
 main = Blueprint("main", __name__)
 
@@ -9,7 +10,9 @@ main = Blueprint("main", __name__)
 def home():
     if current_user.is_authenticated:
         return redirect(url_for("posts.all_posts"))
-    return render_template("main/home.html")
+    posts = Post.objects().order_by("-date_posted")
+    return render_template("main/home.html",
+                           posts=posts)
 
 
 # renders the about page
