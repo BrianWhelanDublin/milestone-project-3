22dded18947c2bce83e26bb63baaac5ca661815d
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-02-06 15:43:29 +0000

    add contact form

---
+++ hello_blog/main/main_forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import (StringField,
+                     SubmitField, TextAreaField)
+from wtforms.validators import DataRequired, Length, Email
+
+
+class ContactForm(FlaskForm):
+    name = StringField("Name",
+                       validators=[DataRequired(),
+                                   Length(min=3, max=20)])
+    email = StringField("Email",
+                        validators=[DataRequired(), Email()])
+    message = TextAreaField("Message", validators=[DataRequired()])
+    submit = SubmitField("Contact us")
