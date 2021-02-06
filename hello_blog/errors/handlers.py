7c1a96c6e0e7cd19366d23959863d505b285b78a
BrianWhelanDublin <brianwhelan1984@yahoo.co.uk>
2021-02-06 15:01:49 +0000

    add comments

--- hello_blog/errors/handlers.py
+++ hello_blog/errors/handlers.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template
 
 
+# create the error blueprint
 errors = Blueprint("errors", __name__)
 
 
+#  create handlers for each error
 @errors.app_errorhandler(404)
 def error_404(error):
     return render_template("errors/404.html"), 404
